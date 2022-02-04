3badb25a5561c6337084f80473272ebea2ce5fd2
Ulinnuha <ulinnuha@dr.com>
2022-02-04 22:46:00 +0700

    modify tree loop
    berhasil hore..

--- loop01.c
+++ loop01.c
@@ -8,6 +8,20 @@ int main (void){
     printf("n = ");
     scanf("%i", &n);
 
-    printf("your n is %i", n);
+    printf("here's yo %i n\n", n);
+
+    for (int z = 0; z < n; z++){
+        
+        for (int x = z; x < n; x++)
+            printf(" ");
+        
+        for (int c = 0; c <= z; c++)
+            printf("*");
+        
+        for (int v = 1; v <= z; v++)
+            printf("*");
+
+        printf("\n");
+    }
 
 }
